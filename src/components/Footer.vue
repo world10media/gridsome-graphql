4e144097439e3223a160019c85613cb66e26975a
Simon Lacey <simonlacey20@gmail.com>
2021-09-27 15:08:03 -0400

    footer vue and styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,28 @@
 <template>
     <footer>
-        <p>Copyright 2021 by MSD</p>
+        <p>&copy; {{getCurrentYear}} by <a href="href">MySleekDesigns</a></p>
     </footer>
 </template>
+
+<script>
+
+export default {
+    data: () => ({
+        href: 'http://www.mysleekdesigns.com'
+    }),
+    computed: {
+        getCurrentYear() {
+            return new Date().getFullYear()
+        }
+    }
+}
+</script>
+
+<style>
+    footer {
+        text-align: center;
+        color: #00a672;
+        letter-spacing: 0.5px;
+        padding: 20px;
+    }
+</style>
